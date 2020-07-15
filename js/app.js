5dab0c3199f4758c3c4260cdafb608b7a6c13ba4
jelson <jelson20@outlook.com>
2020-07-15 22:01:00 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,15 +2,16 @@
 	'use strict';
 
 	let btn = doc.querySelectorAll('.button');
-	//let myBody = doc.querySelector('body');
+	let myBody = doc.querySelector('body');
 	// console.log(btn)
 
 	// change background color
 	function backgroundColors(e) {
 		console.log(e.target);
+		myBody.style.background = e.target.dataset.color;
 	}
 
-	for (let i = 0; btn < i.length; i++) {
+	for (let i = 0; i < btn.length; i++) {
 		btn[i].addEventListener('click', backgroundColors, false);
 	}
 })(window, document);
